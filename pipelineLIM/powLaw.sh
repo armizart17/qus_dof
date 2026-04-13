3331457acbfa8c435fbc35823ad804b5e412d6b4
Edmundo Miranda Zarate <e.a.miranda.zarate@tue.nl>
2026-04-13 11:24:17 +0200

    w4

--- pipelineLIM/powLaw.sh
+++ pipelineLIM/powLaw.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=dof_grid_powLaw
-#SBATCH --output="./slurmout/slurm-%j.out"
-#SBATCH --partition=thinkstation
-#SBATCH --nodelist=worker7
+##SBATCH --output="./slurmout/slurm-%j.out"
+##SBATCH --partition=thinkstation
+##SBATCH --nodelist=worker7
+##SBATCH --nodes=1
+##SBATCH --ntasks=1
+##SBATCH --cpus-per-task=10
+##SBATCH --mem=0
+
+#SBATCH --partition=debug
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-##SBATCH --mem=0
+#SBATCH --gres=gpu:1
+#SBATCH --nodelist=worker4
 
 srun matlab -nosplash -nodesktop -nodisplay -r "\
 cd('./cluster'); \
